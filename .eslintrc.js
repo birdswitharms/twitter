85dcceab5342cd03a4292e502d30a549bc6dfae5
birdswitharms <segal.a@hotmail.com>
2024-01-24 00:54:48 -0500

    Add prettier and tailwindcss to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     extends: [
+        'prettier',
+        'plugin:tailwindcss/recommended',
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier
